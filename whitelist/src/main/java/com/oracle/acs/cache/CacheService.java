38f5099326814c09e791f21a0cbe821f86ffdab2
Ümit KILIÇ <umitkilic@gmail.com>
2024-08-19 09:53:25 +0300

    Update CacheService.java

--- whitelist/src/main/java/com/oracle/acs/cache/CacheService.java
+++ whitelist/src/main/java/com/oracle/acs/cache/CacheService.java
@@ -1,93 +1,62 @@
-package com.oracle.acs.dao;
+package com.oracle.acs.cache;
 
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.oracle.acs.cache.CacheService;
-import com.oracle.acs.model.WhiteListModel;
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
 
 import jakarta.annotation.PostConstruct;
 
 @Component
-public class WhiteListDAO extends BaseDAO {
+public class CacheService {
 
-	@Autowired
-	CacheService cacheService;
+	private NamedCache<String,List<Map<String,Object>>> cacheWhiteList ;
 	
-
-	@Bean
-	public JdbcTemplate jdbcTemplate() {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		return jdbcTemplate;
-	}
-
+	private NamedCache<String, List<Map<String, Object>>> getIpByContext;
+	
+	private NamedCache<String, String> ipAndContextList;
+	
 	@PostConstruct
 	public void init() {
-		
-		List<Map<String, Object>> queryForList = jdbcTemplate().queryForList("SELECT * from WHITE_LIST");
-		cacheService.getCacheWhiteList().put("all",queryForList);
-		
-		Map<String,String> ipAndAddressList = cacheService.getIpAndContextList();
-		
-		for (Map<String, Object> map : queryForList) {
-			System.out.println(map.get("context"));
-			ipAndAddressList.put(map.get("context")+"_"+map.get("ip_address"), map.get("white_list_id").toString());
-		}
+		System.out.println("Initializing Cache Service...");
+		setCacheWhiteList(CacheFactory.getCache("cacheWhileList"));
+		getIpByContext  = CacheFactory.getCache("getIpByContext");
+		setIpAndContextList(CacheFactory.getCache("ipAndContextList"));
 		
 	}
 
-	public List<Map<String, Object>> listIpByContext(String context) {
-		List<Map<String, Object>> queryForList = null;
-		if (cacheService.getGetIpByContext().get(context) == null ) {
-			queryForList = jdbcTemplate().queryForList("SELECT * from WHITE_LIST WHERE context =  ?", context);
-			if (queryForList.size()>0 ) {
-				cacheService.getGetIpByContext().put(context, queryForList);	
-			}
-		} else {
-			queryForList = cacheService.getGetIpByContext().get(context);
-		}
-		
-		return queryForList;
+	public NamedCache<String, List<Map<String, Object>>> getGetIpByContext() {
+		return getIpByContext;
 	}
 
-	public boolean doIHavePermission(String context, String ipAddress, int listenMode) {
-		Integer kacTaneIpAdresiVar = 0;
-		if (listenMode == 1) {
-			saveWhiteListModel(new WhiteListModel(context, ipAddress));
-			return true;
-		}
-		
-		if (cacheService.getIpAndContextList().containsKey(context+"_"+ipAddress)) return true;
-		
-		kacTaneIpAdresiVar = jdbcTemplate().queryForObject("SELECT count(1) from WHITE_LIST WHERE context =  ? and ip_address like ?", Integer.class, context,ipAddress);
-		
-		return kacTaneIpAdresiVar > 0 ? true : false;
+
+
+	public void setGetIpByContext(NamedCache<String, List<Map<String, Object>>> getIpByContext) {
+		this.getIpByContext = getIpByContext;
 	}
 
-	public boolean saveWhiteListModel(WhiteListModel model) {
-		try {
-			int update = jdbcTemplate().update(
-					"INSERT INTO WHITE_LIST (white_list_id, context, ip_address )  VALUES ( SEQ_WHITE_LIST.nextval, ? , ?  ) ",
-					model.getContext(), model.getIpAddress());
-			
-			List queryForList = jdbcTemplate().queryForList("SELECT * from WHITE_LIST WHERE context =  ?", model.getContext());
-			cacheService.getGetIpByContext().put(model.getContext(), queryForList);
-			
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
 
+	public NamedCache<String,List<Map<String,Object>>> getCacheWhiteList() {
+		return cacheWhiteList;
 	}
 
-	public List<Map<String, Object>> listAll() {
-		List<Map<String, Object>> queryForList = jdbcTemplate().queryForList("SELECT * from WHITE_LIST");
-		return queryForList;
+
+
+	public void setCacheWhiteList(NamedCache<String,List<Map<String,Object>>> cacheWhiteList) {
+		this.cacheWhiteList = cacheWhiteList;
 	}
 
+	public NamedCache<String, String> getIpAndContextList() {
+		return ipAndContextList;
+	}
+
+	public void setIpAndContextList(NamedCache<String, String> ipAndContextList) {
+		this.ipAndContextList = ipAndContextList;
+	}
+
+	
+	
 }
